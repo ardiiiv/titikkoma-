b98ec0621b97727384b3ce2d7a7f0d9c81f4bc95
ardiiiv <ardiiiv222@gmail.com>
2025-08-22 05:15:12 +0700

    update footer

--- src/components/Layouts/Footer.jsx
+++ src/components/Layouts/Footer.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className=" bg-gray-700 p-14">
-      <div className="w-11/12 mx-auto flex justify-between text-base text-white">
+      <div className="w-11/12 mx-auto flex md:flex-row flex-col justify-between text-base text-white">
         <div>
           <h1 className="font-bold font-FiraCode text-lg text-orange-500">
             TitikKoma;
           </h1>
         </div>
         <div>
-          <ul className="flex gap-4">
+          <ul className="flex md:flex-row flex-col gap-4">
             <li>
               <Link to="/">Home</Link>
             </li>
